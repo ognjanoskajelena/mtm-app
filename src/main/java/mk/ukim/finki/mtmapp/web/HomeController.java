acafbceee28a93fccdc759e86538de0817d932f5
Jelena Ognjanoska <jognjanoska@yahoo.com>
2022-08-13 01:23:22 +0200

    Modify and remove templates; Add more dummy data

--- src/main/java/mk/ukim/finki/mtmapp/web/HomeController.java
+++ src/main/java/mk/ukim/finki/mtmapp/web/HomeController.java
@@ -9,14 +9,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = {"/", "/home"})
 public class HomeController {
 
-    @GetMapping
-    public String getHomePage(Model model) {
-        model.addAttribute("headTitle", "Home");
-        model.addAttribute("style1", "header.css");
-        model.addAttribute("style2", "home.css");
-        model.addAttribute("bodyContent", "home");
-        return "master-template";
-    }
+//    @GetMapping
+//    public String getHomePage(Model model) {
+//        model.addAttribute("headTitle", "Home");
+//        model.addAttribute("style1", "header.css");
+//        model.addAttribute("style2", "home.css");
+//        model.addAttribute("bodyContent", "home");
+//        return "master-template";
+//    }
 
     @GetMapping("/about-us")
     public String getAboutUsPage(Model model) {
